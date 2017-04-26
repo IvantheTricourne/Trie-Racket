2227945ba88eee9da083dc167b3fc5a77e3d970d
Carl Factora <cfactora93@gmail.com>
2017-04-26 12:13:34 -0400

    kind of fixed get-words; still is a little wrong

--- trie.rkt
+++ trie.rkt
@@ -11,19 +11,22 @@
 (define (make-empty-node)
   (node (make-hash) #f))
 
+(define (set-dict! dict char next-node)
+  (hash-set! dict char next-node))
+
+;; helper function to get node from a char
 (define (get-child char curr)
   (define ref
     (hash-ref (node-dict curr) char #f))
   (if ref ref #f))
 
-(define (set-dict! dict char next-node)
-  (hash-set! dict char next-node))
-
+;; inserts a string into the trie
 (define (insert str start-node)
   (define str-len (string-length str))
   (let loop ([i 0] [curr start-node])
     (cond
-      [(< i str-len)
+      [(= i str-len) (node (make-hash) #t)]
+      [else
        (define dict (node-dict curr))
        (define char (string-ref str i))
        (define next-node
@@ -32,47 +35,52 @@
                (loop (add1 i) next)
                (loop (add1 i) (make-empty-node)))))
        (set-dict! dict char next-node)
-       (node dict (node-end? curr))]
-      [else (node (make-hash) #t)])))
+       (node dict (node-end? curr))])))
 
+;; determines whether the given trie contains a word
 (define (has-word? str start-node)
   (define str-len (string-length str))
   (let loop ([i 0] [curr start-node])
     (cond
-      [(< i str-len)
+      [(= i str-len) (node-end? curr)]
+      [else
        (define char (string-ref str i))
        (define next-node (get-child char curr))
-       (and next-node (loop (add1 i) next-node))]
-      [else (node-end? curr)])))
+       (and next-node (loop (add1 i) next-node))])))
 
+;; returns the sub-trie after a given prefix string
 (define (get-prefix str start-node)
   (define str-len (string-length str))
   (let loop ([i 0] [curr start-node])
     (cond
-      [(< i str-len)
+      [(= i str-len) curr]
+      [else
        (define char (string-ref str i))
        (define next-node (get-child char curr))
-       (and next-node (loop (add1 i) next-node))]
-      [else curr])))
+       (and next-node (loop (add1 i) next-node))])))
 
-
+;; get all words contained within a trie
+;;; todo: Words that stop early (e.g. "cat" and "nice") are repeated
 (define (get-words start-node)
+  (define (add-word word words curr)
+    (if (node-end? curr)
+        (cons (list->string word) words)
+        words))
   (let loop ([word '()] [words '()] [curr start-node])
+    (define dict (node-dict curr))
     (cond
       ;; empty-dict
-      [(zero? (hash-count (node-dict curr)))
+      [(zero? (hash-count dict))
+       ;;(define new-words (add-word word words curr))
+       ;;new-words
        words]
       [else
-       (define dict (node-dict curr))
-       (define new-words
-         (if (node-end? curr)
-             (cons (list->string word) words)
-             words))
+       (define new-words (add-word word words curr))
        (define keys (hash-keys dict))
        (define (loopify c)
-         (define next (hash-ref dict c))
-         (display c) (display (node-dict next)) (newline)
-         (loop (append word (list c)) new-words next))
+         (loop (append word (list c))
+               new-words
+               (hash-ref dict c)))
        (append-map loopify keys)])))
 
 
@@ -82,9 +90,9 @@
   (insert "cat" root-node)
   (insert "cata" root-node)
   (insert "cats" root-node)
+  (insert "nice" root-node)
   (insert "niceuuu" root-node)
   (insert "niceruuu" root-node)
-  ;; when you get rid of this... niceuuu is gone in get-words
   (insert "niceuuuruuu" root-node))
 
 (define root-node (nice-test))
@@ -92,7 +100,7 @@
 (define nice-branch (get-prefix "nice" root-node))
 
 (define (main)
-  ;; check for all words in cat branch
+  ;; tests for has-word?
   (check-true
    (andmap (λ (word) (has-word? word root-node))
            '("cat" "cata" "cats"))
@@ -102,6 +110,11 @@
    (andmap (λ (word) (has-word? word root-node))
            '("niceuuu" "niceruuu" "niceuuuruuu"))
    "missing a word in nice branch")
+  ;; test for get-words
+  ;; (check-true
+  ;;  (andmap (λ (word)
+  ;;            (memv word '("cat" "cata" "cats" "niceuuu" "niceruuu" "niceuuuruuu")))
+  ;;          (get-words root-node)))
   )
 
 
